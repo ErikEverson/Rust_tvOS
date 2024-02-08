e5ea0b682473b29f22cb0a37d8ceb6118d3b9d03
Erik Everson <Erik.Everson@ditto.live>
2024-02-08 09:50:40 -0700

    got hello example to have cdylib issue

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,23 @@
 use::std::*;
 use::core::*;
 use::anyhow::*;
-// use::async::*;
-use::async_fn::*;
-use::backtrace::*;
+use::anyhow::*;
+use::arrayvec::*;
 use::base64::*;
-use::cast::*;
-// use::cfg::*;
+use::blake3::*;
+use::bytes::*;
 use::cfg_eval::*;
-// use::crossbeam::*;
-// use::crossbeam::*;
-// use::extension::*;
-// use::extern::*;
-use::futures::*;
-use::lazy_static::*;
-use::log::*;
+use::digest::*;
 use::macro_rules_attribute::*;
-// use::num::*;
-use::num_cpus::*;
 use::paste::*;
 use::serde::*;
 use::serde_cbor::*;
 use::serde_json::*;
-use::static_assertions::*;
+use::sha2::*;
+use::snap::*;
 use::tap::*;
 use::thiserror::*;
-use::tracing::*;
-use::tracing::*;
-use::tracing::*;
-use::url::*;
-use::with_locals::*;
+use::tokio::*;
 
 pub fn foo() {
     println!("Hallo, Rust library here!")
